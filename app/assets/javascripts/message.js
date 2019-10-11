a3de589a99ef55c699304510bb329c7c86a93c85
haya0325 <hayato.5038fal@gmail.com>
2019-10-11 21:36:03 +0900

    add API-function to message

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(function(){
+  function buildHTML(message){  
+    img = message.image.url !== null ? `<img class="contents__chat__box__message__image" src = ${message.image.url}>` : "";
+    var html = `<div class="contents__chat__box">
+                  <div class="contents__chat__box__top-box">
+                    <div class="contents__chat__box__top-box__name">
+                      ${ message.user_name }
+                    </div>
+                    <div class="contents__chat__box__top-box__time">
+                      ${ message.time }
+                    </div>
+                  </div>
+                  <div class="contents__chat__box__message">
+                    <p class="contents__chat__box__message__text">
+                    ${ message.body }
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+    return html;      
+  }
+
+  $('#new_message').submit(function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false    
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents__chat').append(html);
+      $('.contents__form').val('');
+      $('.contents__chat').animate({scrollTop:$('.contents__chat')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('error');
+    });
+  });
+});
+
